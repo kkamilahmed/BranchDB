6bf940e6fd59ba434ce64f6d4bcb6fc3b094d0ef
kkamilahmed <khajakamilahmed@gmail.com>
2025-12-23 05:13:15 -0500

    Implemented a input buffer for REPL

--- REPL.c
+++ REPL.c
@@ -1,22 +1,71 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    long input_length;  
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+    return input_buffer;
+}
+
+char* toLower(char* input);
+
+void print_prompt() { printf("branchdb > "); }
+
+void read_input(InputBuffer* input_buffer) {
+  long bytes_read =getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+  if (bytes_read <= 0) {
+    printf("Error reading input\n");
+    return ;
+  }
+
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
 
 
 int main() {
     char  input [100];
+    InputBuffer* input_buffer = new_input_buffer();
     while (1) {
-        printf("branchdb > ");
+        print_prompt();
+        read_input(input_buffer);
+        toLower(input_buffer->buffer);
 
-        if (!fgets(input, sizeof(input), stdin)) {
-            printf("\n");
-            break;
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+            close_input_buffer(input_buffer);
+            return 0;
+        } 
+        
+        else {
+        printf("Unrecognized command '%s'.\n", input_buffer->buffer);
         }
+        
 
-        if (strncmp(input, ".exit", 5) == 0) {
-            break;
-        }
 
-        printf("You typed: %s", input);
     }
     return 0;
+}
+
+
+char * toLower(char*input){
+        for (int i = 0; input[i] != '\0'; i++) {
+            input[i] = tolower((unsigned char)input[i]);
+        }
 }
